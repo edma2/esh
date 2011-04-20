ccd8a883472cde5e39adf95bd830b8fe54e3eacc
Eugene Ma <edma2@HackBox.localdomain>
2011-04-20 02:29:38 -0700

    error messages

--- esh.c
+++ esh.c
@@ -1,5 +1,4 @@
 /* esh.c - Eugene's SHell */
-/* TODO: Command redirection doesn't append */
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -10,9 +9,9 @@
 
 #define DIRMAX          100
 #define INPUTMAX        4096
+#define BUFMAX          256 
 #define TOKS            16
 #define CMDS            16
-#define BUFMAX          256 
 
 int builtin_chdir(char *path);
 int builtin_cwd(void);
@@ -33,31 +32,31 @@ int main(void) {
                         break;
                 input[strlen(input)-1] = '\0';
                 if ((fd = parse(input, cmd, INPUTMAX)) < 0) {
-                        fprintf(stderr, "error: parse() failed\n");
+                        fprintf(stderr, "esh: parse() failed\n");
                         cmd_free(cmd);
                         continue;
                 }
                 /* Look at first token */
                 if (strcmp(cmd[0][0], "cd") == 0) {
                         if (cmd[0][2] != NULL || cmd[1][0] != NULL)
-                                fprintf(stderr, "usage: cd [dir]\n");
+                                fprintf(stderr, "esh: usage: cd [dir]\n");
                         else if (builtin_chdir(cmd[0][1]) < 0)
-                                fprintf(stderr, "error: builtin cd failed\n");
+                                fprintf(stderr, "esh: builtin cd failed\n");
                 } else if (strcmp(cmd[0][0], "pwd") == 0) {
                         if (cmd[0][1] != NULL || cmd[1][0] != NULL)
-                                fprintf(stderr, "usage: pwd\n");
+                                fprintf(stderr, "esh: usage: pwd\n");
                         else if (builtin_cwd() < 0) 
-                                fprintf(stderr, "error: builtin pwd failed\n");
+                                fprintf(stderr, "esh: builtin pwd failed\n");
                 } else if (strcmp(cmd[0][0], "exit") == 0) {
                         if (cmd[0][1] != NULL || cmd[1][0] != NULL) {
-                                fprintf(stderr, "usage: exit\n");
+                                fprintf(stderr, "esh: usage: exit\n");
                         } else {
                                 cmd_free(cmd);
                                 return 0;
                         }
                 } else {
                         if (cmd_exec(cmd, fd) < 0)
-                                fprintf(stderr, "error: cmd_exec() failed\n");
+                                fprintf(stderr, "esh: cmd_exec() failed\n");
                 }
                 cmd_free(cmd);
         }
@@ -71,7 +70,6 @@ int builtin_cwd(void) {
 
         if (getcwd(dir, DIRMAX) == NULL)
                 return -1;
-        printf("%s\n", dir);
 
         return 0;
 }
@@ -147,7 +145,7 @@ int parse(char *input, char *cmd[CMDS][TOKS], int n) {
                                                 /* Terminate everything */
                                                 cmd[cmd_index][0] = NULL;
                                                 buf_index = 0;
-                                                state = flag_redirect ? ERROR : REDIRECT;
+                                                state = REDIRECT;
                                         }
                                 } else if (c != ' ') {
                                         /* Begin reading new token into buffer */
@@ -253,19 +251,6 @@ int parse(char *input, char *cmd[CMDS][TOKS], int n) {
         return fd ? fd : 1;
 }
 
-void cmd_free(char *cmd[CMDS][TOKS]) {
-        int cmd_index, tok_index;
-        for (cmd_index = 0; cmd_index < CMDS; cmd_index++) {
-                if (cmd[cmd_index][0] == NULL)
-                        break;
-                for (tok_index = 0; tok_index < TOKS; tok_index++) {
-                        if (cmd[cmd_index][tok_index] == NULL)
-                                break;
-                        free(cmd[cmd_index][tok_index]);
-                }
-        }
-}
-
 int cmd_exec(char *cmd[CMDS][TOKS], int fd) {
         int i, pid;
         int even[2], odd[2];
@@ -277,38 +262,29 @@ int cmd_exec(char *cmd[CMDS][TOKS], int fd) {
                 p0 = i & 1 ? odd : even;
                 p1 = i & 1 ? even : odd;
                 /* Make a new pipe child will write to */
-                if (pipe(p0) < 0) {
-                        fprintf(stderr, "error: pipe() failed\n");
+                if (pipe(p0) < 0)
                         return -1;
-                }
                 pid = fork();
                 if (pid == 0) {
                         /* Read from previous pipe */
                         if (i > 0) {
                                 close(p1[1]);
-                                if (dup2(p1[0], 0) < 0) {
-                                        fprintf(stderr, "error: dup2() failed\n");
+                                if (dup2(p1[0], 0) < 0)
                                         exit(-1);
-                                }
                         }
                         /* Write to new pipe */
                         if (cmd[i+1][0] != NULL) {
                                 close(p0[0]);
-                                if (dup2(p0[1], 1) < 0) {
-                                        fprintf(stderr, "error: dup2() failed\n");
+                                if (dup2(p0[1], 1) < 0)
                                         exit(-1);
-                                }
                         } else {
                                 /* Last iteration - write to specified file */
-                                if (dup2(fd, 1) < 0) {
-                                        fprintf(stderr, "error: dup2() failed\n");
+                                if (dup2(fd, 1) < 0)
                                         exit(-1);
-                                }
                         }
                         /* Replace with new image in child process */
                         if (execvp(*cmd[i], cmd[i]) < 0) {
-                                printf("%s\n", *cmd[i]);
-                                fprintf(stderr, "error: execvp() failed\n");
+                                fprintf(stderr, "esh: command not found: %s\n", *cmd[i]);
                                 exit(-1);
                         }
                 } else if (pid > 0) {
@@ -323,4 +299,17 @@ int cmd_exec(char *cmd[CMDS][TOKS], int fd) {
         while (wait(NULL) != pid)
                 ;
         return 0;
+}
+
+void cmd_free(char *cmd[CMDS][TOKS]) {
+        int cmd_index, tok_index;
+        for (cmd_index = 0; cmd_index < CMDS; cmd_index++) {
+                if (cmd[cmd_index][0] == NULL)
+                        break;
+                for (tok_index = 0; tok_index < TOKS; tok_index++) {
+                        if (cmd[cmd_index][tok_index] == NULL)
+                                break;
+                        free(cmd[cmd_index][tok_index]);
+                }
+        }
 }
